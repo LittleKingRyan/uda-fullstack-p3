402ddce2acbc67f89165bcd3568ce38ce3933910
RyanWorks <ryanworks@diweihaodeMacBook-Pro.local>
2016-12-20 23:53:41 +1100

    add lib folder with bleach installed update model.py and postpage.html to try rendering again

--- model.py
+++ model.py
@@ -1,11 +1,25 @@
 import os
 import jinja2
 from google.appengine.ext import db
+from bleach import clean
+from markupsafe import Markup
 
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+
+""" This is a solution from stack overflow:
+http://stackoverflow.com/questions/8976683/jinja2-escape-all-html-but-img-b-etc
+"""
+
+def do_clean(text, **kw):
+    """Perform clean and return a Markup object to mark the string as safe.
+    This prevents Jinja from re-escaping the result."""
+    return Markup(clean(text, **kw))
+
+jinja_env.filters['clean'] = do_clean
 
 
 def render_str(template, **params):
